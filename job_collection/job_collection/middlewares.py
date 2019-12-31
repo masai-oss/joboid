b5563523405cffc110236b9f2d275c0f6c35820e
codehia <ach4825@gmail.com>
2019-12-31 14:28:02 +0530

    master added master scraper

--- job_collection/job_collection/middlewares.py
+++ job_collection/job_collection/middlewares.py
@@ -53,7 +53,7 @@ class JobCollectionSpiderMiddleware(object):
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class JobCollectionDownloaderMiddleware(object):
@@ -100,4 +100,4 @@ class JobCollectionDownloaderMiddleware(object):
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
